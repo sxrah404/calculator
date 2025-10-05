ad6cee831d13e3e5bcefb3e7f3cab2ee8670897a
Sarah Jabian <142950731+sxrah404@users.noreply.github.com>
2025-10-05 17:43:43 -0500

    basic setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,38 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:desktop_window/desktop_window.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    await DesktopWindow.setWindowSize(const Size(500, 850));
+  }
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: CalculatorApp()));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class CalculatorApp extends StatefulWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  State<CalculatorApp> createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
+  ThemeMode currentThemeMode = ThemeMode.light;
+
+  void toggleTheme() {
+    setState(() {
+      currentThemeMode = currentThemeMode == ThemeMode.light
+          ? ThemeMode.dark
+          : ThemeMode.light;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
